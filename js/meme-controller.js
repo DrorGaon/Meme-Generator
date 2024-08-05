fb5081f7be2cbfbe2fc56b0a37c48a2d760de670
DrorGaon <drordtuii@gmail.com>
2024-08-05 16:05:13 +0300

    Add basic meme editing

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,19 +1,26 @@
+'use strict'
+
 let gElCanvas
 let gCtx
 
 function onInit(){
     const elGallery = document.querySelector('.gallery')
     let strHTML = ''
-    for(let i = 1; i <= 18; i++){
-        strHTML += `
-        <img onclick="onSelectImage('${i}')" 
-        src="img/${i}.jpg" alt="">
+    getImgs().map(({url, id}) => {
+        strHTML+= `<img onclick="onSelectImage('${id}')" 
+        src="${url}" alt="">
         `
-    }
+    })
+
     elGallery.innerHTML = strHTML
 
     gElCanvas = document.querySelector('.main-canvas')
     gCtx = gElCanvas.getContext('2d')
+}
+
+function loadGallery(){
+    document.querySelector('.main-editor').classList.add('hidden')
+    document.querySelector('.main-gallery').classList.remove('hidden')
 }
 
 function onSelectImage(idx){
@@ -22,5 +29,38 @@ function onSelectImage(idx){
 
     const elImg = new Image()
     elImg.src = `img/${idx}.jpg`
+    elImg.onload = () => {
+        gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+    }
+    changeMeme('image', idx)
+}
+
+function changeColor({target}){
+    changeMeme('text-color', target.value)
+    renderMeme()   
+}
+
+function editText({target}){
+    changeMeme('text', target.value)
+    const meme = getMeme()
+    renderMeme()   
+}
+
+function renderMeme(x, y){
+    const meme = getMeme()
+    const {text, color} = meme.lines[meme.selectedLineIdx]
+    const elImg = new Image()
+    elImg.src = `img/${meme.selectedImgId}.jpg`
     gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+
+    gCtx.beginPath()
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = color
+    gCtx.font = '60px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+
+    gCtx.fillText(text, gElCanvas.width / 2, gElCanvas.height / 2)
+    gCtx.strokeText(text, gElCanvas.width / 2, gElCanvas.height / 2)
 }
