35944efa8dfa6cc0bf86577be156c683264e2207
DrorGaon <drordtuii@gmail.com>
2024-08-05 14:37:30 +0300

    Add image gallery and implement image select

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -2,14 +2,25 @@ let gElCanvas
 let gCtx
 
 function onInit(){
+    const elGallery = document.querySelector('.gallery')
+    let strHTML = ''
+    for(let i = 1; i <= 18; i++){
+        strHTML += `
+        <img onclick="onSelectImage('${i}')" 
+        src="img/${i}.jpg" alt="">
+        `
+    }
+    elGallery.innerHTML = strHTML
+
     gElCanvas = document.querySelector('.main-canvas')
     gCtx = gElCanvas.getContext('2d')
-    restartCanvas()
 }
 
-function restartCanvas(){
-    gCtx.beginPath()
-    gCtx.fillStyle = 'white'
-    gCtx.rect(0, 0, gElCanvas.width, gElCanvas.height)
-    gCtx.fill()
+function onSelectImage(idx){
+    document.querySelector('.main-editor').classList.remove('hidden')
+    document.querySelector('.main-gallery').classList.add('hidden')
+
+    const elImg = new Image()
+    elImg.src = `img/${idx}.jpg`
+    gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
 }
